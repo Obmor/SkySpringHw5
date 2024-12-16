b19e5700963e359bc72a80350ac6b7470684d36b
Obmor <terekhin.alex@mail.ru>
2024-12-16 12:44:32 +0300

    TheFirstLook

---
+++ src/main/java/pro/sky/skyspringhw5/SkySpringHw5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyspringhw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySpringHw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySpringHw5Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyspringhw5/SkySpringHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyspringhw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySpringHw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
